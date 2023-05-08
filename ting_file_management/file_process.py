2176b70ade236515b58ec852ef5306e67a42c945
pedrohassen <pedro_hasse@yahoo.com.br>
2023-05-08 17:20:58 -0300

    feat: 🦾 Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,23 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+
+    list_data = [
+        instance.search(i)["nome_do_arquivo"] for i in range(len(instance))
+    ]
+
+    if path_file not in list_data:
+        processed_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(data),
+            "linhas_do_arquivo": data,
+        }
+
+        instance.enqueue(processed_data)
+        sys.stdout.write(str(processed_data))
 
 
 def remove(instance):
