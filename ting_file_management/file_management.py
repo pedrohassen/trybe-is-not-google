cff57c22a3954a0fa3680dbfa9f6a6b91703444e
pedrohassen <pedro_hasse@yahoo.com.br>
2023-05-08 16:49:23 -0300

    feat: 🦾 Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            raise TypeError
+        with open(path_file, 'r') as file:
+            return file.read().split('\n')
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    except TypeError:
+        sys.stderr.write('Formato inválido')
